b852d374422c5bb9c7670af2d66aaa9827086aa9
Tim Leland <timothy.leland@gmail.com>
2019-06-11 19:07:23 -0400

    Add shortcut to save tab

--- assets/js/backgroundScript.js
+++ assets/js/backgroundScript.js
@@ -1,55 +1,62 @@
 var saveLink = function(info) {
-    var urlToSave = info.linkUrl ? info.linkUrl : info.pageUrl;
-    $.ajax({
-        url: urlToSave,
-        success: function(data) {
-            var title = data.match(/<title>(.*)<\/title>/);
-            if (title !== null) {
-                var siteTitle = title[0].replace('<title>', '').replace('</title>', '');
-            } else {
-                var siteTitle = urlToSave;
-            }
-            var siteTitle = $("<div/>").html(siteTitle).text();
-            console.log(siteTitle);
+	var urlToSave = info.linkUrl ? info.linkUrl : info.pageUrl;
+	$.ajax({
+		url: urlToSave,
+		success: function(data) {
+			var title = data.match(/<title>(.*)<\/title>/);
+			if (title !== null) {
+				var siteTitle = title[0].replace('<title>', '').replace('</title>', '');
+			} else {
+				var siteTitle = urlToSave;
+			}
+			var siteTitle = $('<div/>')
+				.html(siteTitle)
+				.text();
+			console.log(siteTitle);
 
-            var link = {
-                url: urlToSave,
-                title: siteTitle,
-                isRead: 0
-            };
+			var link = {
+				url: urlToSave,
+				title: siteTitle,
+				isRead: 0
+			};
 
-            addUpdateLink(link);
-        }
-    });
+			addUpdateLink(link);
+		}
+	});
 };
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        window.open('https://timleland.com/read-later-extension/');
-    } else if (details.reason == "update") {
-
-    }
+	if (details.reason == 'install') {
+		window.open('https://timleland.com/read-later-extension/');
+	} else if (details.reason == 'update') {
+	}
 });
 
 chrome.contextMenus.create({
-    title: "Save Link",
-    type: "normal",
-    contexts: ["link", "page"],
-    onclick: saveLink
+	title: 'Save Link',
+	type: 'normal',
+	contexts: ['link', 'page'],
+	onclick: saveLink
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-    if (command === 'save-tab') {
-        chrome.tabs.query({
-            active: true,
-            windowType: "normal",
-            currentWindow: true
-        }, function(tabs) {
-            var info = {
-                linkUrl: tabs[0].url
-            }
+	if (command === 'save-tab') {
+		chrome.tabs.query(
+			{
+				active: true,
+				currentWindow: true
+			},
+			function(tabs) {
+				if (!tabs || !tabs.length) {
+					return;
+				}
 
-            saveLink(info);
-        })
-    }
+				var info = {
+					linkUrl: tabs[0].url
+				};
+
+				saveLink(info);
+			}
+		);
+	}
 });
